b7fb53bc96c79ac3852fc4e2ed414a2edd47d7b8
Vaishnavi610 <70099658+Vaishnavi610@users.noreply.github.com>
2021-03-30 12:45:25 +0530

    update

--- owner/urls.py
+++ owner/urls.py
@@ -3,4 +3,10 @@ from . import views as views
 
 urlpatterns = [
     path("", views.index, name="index"),
+     path("add_member/", views.add_member, name="add_member"),
+    path("pay_history/", views.pay_history, name="pay_history"),
+    path("menu", views.menu, name="menu")
+    
+    
+    
 ]
