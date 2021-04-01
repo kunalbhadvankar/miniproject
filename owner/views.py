b88d1d8409a17727da4543d3741d8e6fd4b654e4
Vaishnavi610 <70099658+Vaishnavi610@users.noreply.github.com>
2021-04-01 11:41:41 +0530

    update login view

--- owner/views.py
+++ owner/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import *
 
+
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms  import UserCreationForm
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import *
+from django.contrib import messages
+from django.urls import reverse
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username =  request.POST.get('username')
+        password = request.POST.get('password') 
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            messages.info(request, 'username or password is incorrect')
+                  
+    return render(request, "owner/login.html")
 
 def main(request):
+    
     return render(request, "owner/layout.html")
+
+
+
 
 def add_member(request):
     return render(request, "owner/add_member.html")
@@ -15,4 +37,6 @@ def pay_history(request):
 
 def menu(request):
     return render(request, "owner/menu.html")
+def logout(request):
+    pass
 
