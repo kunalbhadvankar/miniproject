eb3addefc96968d17998cb24772dc66924834875
Kunal Sanjay Bhadvnkar <bhadvankarkunal@gmail.com>
2021-04-01 16:22:05 +0530

    updated tables transactions and others also by making migration

--- owner/admin.py
+++ owner/admin.py
@@ -1,21 +1,22 @@
-from .models import User,History,Menu,Transaction
+from .models import User, Payment, Menu, Transaction
 
 from django.contrib import admin
 # Register your models here.
 
+
 class MenuAdmin(admin.ModelAdmin):
-    list_display=("Menu_id","Name","Price")
+    list_display = ("Menu_id", "Name", "Price")
+
 
 class UserAdmin(admin.ModelAdmin):
-    list_display= ("Name", "Email", "Mobile", "Roll", "Username",)
+    list_display = ("Name", "Email", "Mobile", "Roll", "Username",)
+
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display=("Member_id","Menu_id","Quantity")
-
+    list_display = ("Member_id", "Menu_id", "Quantity")
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(History)
+admin.site.register(Payment)
 admin.site.register(Menu, MenuAdmin)
-admin.site.register(Transaction,TransactionAdmin)
-
+admin.site.register(Transaction, TransactionAdmin)
--- owner/models.py
+++ owner/models.py
@@ -3,33 +3,44 @@ from django.db import models
 # Create your models here
 
 
-
-
 class User(models.Model):
-    Name = models.CharField(max_length=64,null=True)
-    Email = models.EmailField(max_length=64,null=True)
-    Mobile= models.IntegerField()
-    Roll = models.CharField(max_length=20,null=True)
-    Username=models.CharField(max_length=64,null=True)   
+    CATEGORY = (
+        ('Owner', 'Owner'),
+        ('Staff', 'Staff'),
+        ('Member', 'Member')
+    )
+    Name = models.CharField(max_length=64, null=True)
+    Email = models.EmailField(max_length=64, null=True)
+    Mobile = models.IntegerField()
+    Roll = models.CharField(max_length=20, null=True, choices=CATEGORY)
+    Username = models.CharField(max_length=64, null=True)
 
-class History(models.Model):
-    Member_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
-   
 class Menu(models.Model):
-    CATEGORY=(
-        ('Beverages','Beverages'),
-        ('Thali','Thali'),
-        ('snaks', 'snacks')
+    CATEGORY = (
+        ('Snacks', 'Snacks'),
+        ('Cold_Drink', 'Cold_Drink'),
+        ('Veg', 'Veg'),
+        ('Non-Veg', 'Non-Veg')
     )
     Menu_id = models.AutoField(primary_key=True)
-    Name= models.CharField(max_length=50, null=True)
-    Price= models.IntegerField(default=1, null=True)
-    category= models.CharField(max_length=50, null=True, choices=CATEGORY)
+    Name = models.CharField(max_length=50, null=True)
+    Price = models.IntegerField(default=1, null=True)
+    category = models.CharField(max_length=50, null=True, choices=CATEGORY)
+
 
 class Transaction(models.Model):
     Member_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     Menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE, null=True)
-    date_added=models.DateTimeField(auto_now_add=True,null=True)
-    Quantity=models.IntegerField(default=1)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+    Quantity = models.IntegerField(default=1)
 
+
+class Payment(models.Model):
+    CATEGORY = (
+        ('Advance', 'Advance'),
+        ('Due', 'Due')
+    )
+    Member_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    Amount_paid = models.IntegerField(default=1, null=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
