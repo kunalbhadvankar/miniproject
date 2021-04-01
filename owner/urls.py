0c185158b3d899dbf5d3545ab83dad64d081947c
Vaishnavi610 <70099658+Vaishnavi610@users.noreply.github.com>
2021-04-01 11:42:39 +0530

    add new path

--- owner/urls.py
+++ owner/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views as views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-     path("add_member/", views.add_member, name="add_member"),
+    path("", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+    path("main/", views.main, name="main"),
+    path("add_member/", views.add_member, name="add_member"),
     path("pay_history/", views.pay_history, name="pay_history"),
     path("menu", views.menu, name="menu")
     
