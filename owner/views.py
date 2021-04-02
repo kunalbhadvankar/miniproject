9af9af460672d216c19ac8a9f37b19fae0a459e3
Vaishnavi610 <70099658+Vaishnavi610@users.noreply.github.com>
2021-04-02 09:29:51 +0530

    add decorators

--- owner/views.py
+++ owner/views.py
@@ -7,27 +7,37 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user, allowed_users, admin_only
 # Create your views here.
 
-
+@login_required(login_url='login')
+@admin_only
 def main(request): 
     return render(request, "owner/layout.html")
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def add_member(request):
     return render(request, "owner/add_member.html")
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def pay_history(request):
     return render(request, "owner/history.html")
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def menu(request):
     menu = Menu.objects.all()
     return render(request, "owner/menu.html",{'menu': menu})
 
-
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def home(request):
     pass
 
-
+@unauthenticated_user
 def login_owner(request):
     if request.method == 'POST':
         username=request.POST.get('username')
