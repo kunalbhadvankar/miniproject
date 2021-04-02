1102275a41fd28589825a31c7bdd6b18b8d446d7
Kunal Sanjay Bhadvnkar <bhadvankarkunal@gmail.com>
2021-04-02 10:07:03 +0530

    ky mhit

--- members/urls.py
+++ members/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views as views
 
 urlpatterns = [
+    path("",views.main,name="main"),
+  
+
 ]
--- members/views.py
+++ members/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import *
+from django.contrib import messages
+from django.urls import reverse
 
 # Create your views here.
 
 
 def main(request):
-    return render(request, "members/main.html")
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse("login"))
+    return render(request,"members/main.html")
 
 
